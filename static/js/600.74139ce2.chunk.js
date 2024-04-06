0fd25001b69a9a4f72aa7a822b87b780f1911696
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-04-06 09:10:27 +0000

    Deploying to gh-pages from @ StudentVlad5/store-of-stylish-clothes@723e23e6928239d26a90348912f767a00ce363e4 🚀

---
+++ static/js/600.74139ce2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkstore_of_stylish_clothes=self.webpackChunkstore_of_stylish_clothes||[]).push([[600],{1875:function(o,s,t){t.r(s);var e=t(2791),l=t(7689),n=t(4116),r=t(2296),c=t(9434),f=t(456),i=t(3329);s.default=function(){var o=(0,l.TH)(),s=(0,c.I0)();return window.scrollTo({top:0,left:0,behavior:"smooth"}),(0,e.useEffect)((function(){s((0,f.T)())}),[o]),(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)(n.H,{title:"Shop",description:"Catalog of goods"}),(0,i.jsx)(r.g,{})]})}}}]);
+//# sourceMappingURL=600.74139ce2.chunk.js.map

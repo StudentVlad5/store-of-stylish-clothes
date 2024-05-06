77e911fcf76b12efc8a7359ee88048252e2d00a6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2024-05-06 07:23:59 +0300

    remove snap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { hydrateRoot } from 'react-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
@@ -12,56 +11,22 @@ import { StatusProvider } from 'components/ContextStatus/ContextStatus';
 import i18n from './utils/i18n';
 // import { getTemporaryToken } from 'token';
 
-const rootElement = document.getElementById('root'); // (или ваш id при олтличии)
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    <Provider store={store}>
-      <PersistGate loading={'Loading'} persistor={persistor}>
-        <BrowserRouter basename="/">
-          <StatusProvider>
-            <I18nextProvider i18n={i18n}>
-              <GlobalStyle />
-              <App />
-            </I18nextProvider>
-          </StatusProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>,
-    rootElement,
-  );
-} else {
-  ReactDOM.createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
-      <PersistGate loading={'Loading'} persistor={persistor}>
-        <BrowserRouter basename="/">
-          <StatusProvider>
-            <I18nextProvider i18n={i18n}>
-              <GlobalStyle />
-              <App />
-            </I18nextProvider>
-          </StatusProvider>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>,
-  );
-}
-
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   // <React.StrictMode>
-//   <Provider store={store}>
-//     <PersistGate loading={'Loading'} persistor={persistor}>
-//       <BrowserRouter basename="/">
-//         <StatusProvider>
-//           <I18nextProvider i18n={i18n}>
-//             <GlobalStyle />
-//             <App />
-//           </I18nextProvider>
-//         </StatusProvider>
-//       </BrowserRouter>
-//     </PersistGate>
-//   </Provider>,
-//   // </React.StrictMode>,
-// );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={'Loading'} persistor={persistor}>
+      <BrowserRouter basename="/">
+        <StatusProvider>
+          <I18nextProvider i18n={i18n}>
+            <GlobalStyle />
+            <App />
+          </I18nextProvider>
+        </StatusProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>,
+  // </React.StrictMode>,
+);
 
 // const storedToken = getTemporaryToken();
 
